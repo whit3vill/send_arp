cba2ee24d795783969954580ce0775cbfe7baa3b
whit3vill <whit3vill@gmail.com>
2017-10-10 08:21:15 -0700

    Add send_arp modules

--- send_arp.c
+++ send_arp.c
@@ -26,7 +26,40 @@ void GetIP(u_int8_t *ip_addr, u_int8_t *interface) {
     memcpy(ip_addr, &((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr, IP_ADDR_LEN);
 }
 
-void MakeARP(u_int8_t *packet, u_int8_t *src_mac, u_int8_t *dst_mac, u_int8_t *src_ip, u_int8_t *dst_ip, u_int16_t opcode) {
+void printMAC(u_int8_t *mac_addr) {
+	int i;
+
+	for(i=0;i<6;i++) {
+		printf("%02x:", mac_addr[i]);
+		if(i == 4)	printf("%02x\n", mac_addr[i++]);
+	}
+}
+
+void ARPBroadcast(u_int8_t *packet, u_int8_t *src_mac, u_int8_t *src_ip, u_int8_t *dst_ip, u_int16_t opcode) {
+	struct arp_packet *apkt;
+
+	apkt = (struct arp_packet *)malloc(sizeof(struct arp_packet));
+
+	memcpy(apkt->ether_dhost, "\xff\xff\xff\xff\xff\xff", ETHER_ADDR_LEN);
+	memcpy(apkt->ether_shost, src_mac, ETHER_ADDR_LEN);
+	apkt->ether_type = htons(ETHERTYPE_ARP);
+
+	apkt->hw_type = htons(ARPHRD_ETHER);
+	apkt->ip_type = htons(ETHERTYPE_IP);
+	apkt->hw_len  = ETHER_ADDR_LEN;
+	apkt->ip_len  = IP_ADDR_LEN;
+	apkt->opcode  = htons(opcode);
+
+	memcpy(apkt->src_hw_addr, src_mac, ETHER_ADDR_LEN);
+	memcpy(apkt->src_ip_addr, src_ip, IP_ADDR_LEN);
+	memcpy(apkt->dst_hw_addr, "\x00\x00\x00\x00\x00\x00", ETHER_ADDR_LEN);
+	memcpy(apkt->dst_ip_addr, dst_ip, IP_ADDR_LEN);
+
+	memcpy(packet, epkt, sizeof(struct arp_packet));
+	free(apkt);
+}
+
+void ARPReply(u_int8_t *packet, u_int8_t *src_mac, u_int8_t *dst_mac, u_int8_t *src_ip, u_int8_t *dst_ip, u_int16_t opcode) {
 	struct arp_packet *apkt;
 
 	apkt = (struct arp_packet *)malloc(sizeof(struct arp_packet));
@@ -47,5 +80,6 @@ void MakeARP(u_int8_t *packet, u_int8_t *src_mac, u_int8_t *dst_mac, u_int8_t *s
 	memcpy(apkt->dst_ip_addr, dst_ip, IP_ADDR_LEN);
 
 	memcpy(packet, epkt, sizeof(struct arp_packet));
+
 	free(apkt);
 }
